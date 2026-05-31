9f8108eaee58a8eb5eb0a6cf5340fea58c15bb16
udavikhin <anatoly.udavikhin@gmail.com>
2026-05-31 07:29:24 +0300

    add middleware setting content-type header for api routes

--- internal/api/routes/router.go
+++ internal/api/routes/router.go
@@ -14,7 +14,10 @@ func NewRouter(handlers *handler.Handlers) http.Handler {
 	apiV1.HandleFunc("GET /offers/{id}", handlers.Offer.GetOffer)
 	apiV1.HandleFunc("POST /offers", handlers.Offer.CreateOffer)
 
-	r.Handle("/api/v1/", http.StripPrefix("/api/v1", apiV1))
+	r.Handle(
+		"/api/v1/",
+		http.StripPrefix("/api/v1", middleware.Chain(middleware.ContentTypeJsonMiddleware)(apiV1)),
+	)
 
 	return middleware.Chain(middleware.LoggingMiddleware)(r)
 }
