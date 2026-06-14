b663ccec3f3d3a08a44789d13180f5827b09674e
udavikhin <anatoly.udavikhin@gmail.com>
2026-06-15 02:01:10 +0300

    fix refresh tokens table expires_at typo

--- database/migrations/000003_create_refresh_tokens_table.up.sql
+++ database/migrations/000003_create_refresh_tokens_table.up.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS refresh_tokens (
     id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(id),
     token_hash TEXT UNIQUE NOT NULL,
-    expries_at TIMESTAMP NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
