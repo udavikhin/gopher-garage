60178fb1117cba86085e67e687115f050917c4fc
udavikhin <anatoly.udavikhin@gmail.com>
2026-06-25 01:25:28 +0300

    update footer contents

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,18 +1,20 @@
+import {Link} from "react-router-dom";
+
 const Footer = () => {
     return (
         <footer className="site-footer">
             <div className="site-footer__inner">
-                <a className="brand brand--sm brand--inverse" href="index.html">
+                <Link className="brand brand--sm brand--inverse" to="/">
                     <span className="brand__mark"><svg className="icon"><use
                         href="/assets/icons/sprite.svg#i-car"/></svg></span>
                     <span className="brand__wordmark">Gopher Garage</span>
-                </a>
+                </Link>
                 <span
-                    className="site-footer__copy">© 2026 · Выпускной проект на Go · Выполнен в рамках курса "Разработчик полного цикла (Fullstack) на языке Голанг (Golang)"</span>
+                    className="site-footer__copy">© {(new Date()).getFullYear()} · Выпускной проект на Go + React · Выполнен в рамках курса "Fullstack-разработчик на языке Golang"</span>
                 <nav className="site-footer__links">
-                    <a href="help.html">Помощь</a>
-                    <a href="help.html#contacts">Контакты</a>
-                    <a href="https://github.com" rel="noopener">GitHub</a>
+                    <Link to="/help">Помощь</Link>
+                    <Link to="/help#contacts">Контакты</Link>
+                    <a href="https://github.com/udavikhin" rel="noopener">GitHub</a>
                 </nav>
             </div>
         </footer>
