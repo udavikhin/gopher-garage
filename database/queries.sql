577451cb3447d78450ecf56431a50c1d9c2be309
udavikhin <anatoly.udavikhin@gmail.com>
2026-06-25 02:20:45 +0300

    update sql queries to comply with updated migrations

--- database/queries.sql
+++ database/queries.sql
@@ -5,7 +5,9 @@ SELECT * FROM offers;
 SELECT * FROM offers WHERE id = $1;
 
 -- name: AddOffer :one
-INSERT INTO offers (user_id, vehicle, description, price) VALUES ($1, $2, $3, $4) RETURNING id;
+INSERT INTO offers (user_id, make, model, gearbox, mileage, color, fuel, price, negotiable, description)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+RETURNING id;
 
 -- name: RemoveOffer :exec
 DELETE FROM offers WHERE id = $1;
@@ -14,7 +16,13 @@ DELETE FROM offers WHERE id = $1;
 SELECT * FROM users WHERE email = $1;
 
 -- name: AddUser :one
-INSERT INTO users (email, first_name, last_name, patronymic, password) VALUES ($1, $2, $3, $4, $5) RETURNING id;
+INSERT INTO users (email, full_name, password) VALUES ($1, $2, $3) RETURNING id;
 
 -- name: AddRefreshToken :one
 INSERT INTO refresh_tokens (user_id, token_hash, expires_at) VALUES ($1, $2, $3) RETURNING id;
+
+-- name: GetRefreshTokenByHash :one
+SELECT * FROM refresh_tokens WHERE token_hash = $1 AND expires_at > NOW();
+
+-- name: DeleteRefreshTokenByHash :exec
+DELETE FROM refresh_tokens WHERE token_hash = $1;
